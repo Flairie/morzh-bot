d1a34356579510cb19e002317368a2b5728b7e6d
Flairie <37420327+Flairie@users.noreply.github.com>
2018-06-27 11:00:36 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -41,7 +41,7 @@ client.on('message', msg => {
   for(i = 0; i < sMsg.length; i++) {
     if(sMsg[i] in TempMemr) {
       var INDEX = TempMemr.find(sMsg[i]);
-      msg.channel.send((i + "")+") "+(INDEX+""));
+      msg.reply((i + "")+") "+(INDEX+""));
       var MEMR = TempMemr[INDEX];
       var ALGN = TempAlgn[INDEX];
       var TEXT = TempText[INDEX];
